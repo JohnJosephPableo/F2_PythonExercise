46dbf75d6fc90969afd2085cbba88eafd8641648
terraknighty <johnjosephpableo@gmail.com>
2023-09-04 14:23:28 +0800

    Number 3 and 4 Answered

--- main.py
+++ main.py
@@ -22,6 +22,30 @@ def find_max(a, b, c):
     else:
         print("Maximum number is ", c)
 
+def counts_digits(number):
+    max = 0
+    min = 9
+    count = 0
+    while number > 0:
+        digit = int(number % 10)
+        if digit > max:
+            max = digit
+        if digit < min:
+            min = digit
+        count += 1
+        number = int(number/10)
+    print("Number of digits in number: ", count)
+    print("Smallest: ", min)
+    print("Highest: ", max)
+
+def sum_of_num(number):
+    sum = 0
+    for i in range(1, number+1):
+        sum += i
+    print("Sum: ", sum)
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Number 1
@@ -32,5 +56,14 @@ if __name__ == '__main__':
     b = int(input("Enter second number: "))
     c = int(input("Enter third number: "))
     find_max(a, b, c)'''
+    # Number 3
+    '''num = int(input("Enter a number: "))
+    counts_digits(num)'''
+    # Number 4
+    '''number = int(input("Enter number: "))
+    sum_of_num(number)'''
+    # Number 5
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
