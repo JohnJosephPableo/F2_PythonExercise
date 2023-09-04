af3a5d65ce2a04addb667d8281de1c2c0a160cab
terraknighty <johnjosephpableo@gmail.com>
2023-09-04 13:48:53 +0800

    Number 1 and Number 2 Answered

--- main.py
+++ main.py
@@ -7,10 +7,30 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def convert_temp(celsius):
+    fahrenheit = (celsius*9/5) + 32
+    print("Fahrenheit: ", "{:.2f}".format(fahrenheit))
 
+def find_max(a, b, c):
+    if a > b:
+        if a > c:
+            print("Maximum  number is ", a)
+        else:
+            print("Maximum  number is ", c)
+    elif b > c:
+        print("Maximum number is ", b)
+    else:
+        print("Maximum number is ", c)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Number 1
+    '''celsius = float(input("Enter Celsius: "))
+    convert_temp(celsius)'''
+    # Number 2
+    '''a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    c = int(input("Enter third number: "))
+    find_max(a, b, c)'''
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
